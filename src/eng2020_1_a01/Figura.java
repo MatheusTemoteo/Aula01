44bf38a67e9d06adacd20155b6b9f43fa8fa56c8
819230574 <819230574@MOOLI10105.animaedu.intranet>
2020-03-11 17:52:17 -0300

    Aula 01

---
+++ src/eng2020_1_a01/Figura.java
@@ -0,0 +1,16 @@
+package eng2020_1_a01;
+
+public  abstract class Figura {
+
+	public Figura() {
+		
+	}
+
+	public abstract double area();
+
+	public double perimetro() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
